f52cf12f1794ea0fea094b06d8e368f4f3ae17ed
Lblenner <louis.blenner@gmail.com>
2020-04-01 11:40:22 +0200

    init front project

---
+++ front/pages/about.tsx
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const f = () => {
+    var a = 3
+}
+export default function About() {
+  return (
+    <Layout>
+      <p>This is the about page</p>
+    </Layout>
+  );
+}
