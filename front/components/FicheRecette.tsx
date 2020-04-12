ea0709620e7f241c063403e1667ce833a9347144
melisisse <camilledufour23@gmail.com>
2020-04-12 15:14:08 +0200

    Début affichage fiche recette

--- front/components/FicheRecette.tsx
+++ front/components/FicheRecette.tsx
@@ -43,29 +43,97 @@ export default class FicheRecette extends React.Component<MyProps, MyState> {
       return <CircularProgress />
     }
 
+    const nbPartsInitiale = "8";
+    const pourcentageIngredients = 25;
+    const espaceIngredientsRecette = 10;
+
     const r = this.state.recette
-    var ing = []
+    var listIng = [];
     for (let i = 0; i < r.elements.length; i++) {
       let elem = r.elements[i]
-      ing.push(<p key={elem.id}>{elem.quantite.nombre+" " +elem.quantite.unite+ " "+elem.ingredient.nom}</p>)
+      listIng.push(<li key={elem.id}>{elem.quantite.nombre+" " +elem.quantite.unite+ " "+elem.ingredient.nom}</li>)
     }
+    /*var ing = [];
+      for (let i = 0; i < r.elements.length; i++) {
+      let elem = r.elements[i]
+      ing.push(<p key={elem.id}>{elem.quantite.nombre+" " +elem.quantite.unite+ " "+elem.ingredient.nom}</p>)
+    }*/
 
     return (
       <div id="fiche_container">
         <h1>{r.nom}</h1>
-        {ing}
+        <h6 id="nomPersonne">
+          <a href="/profil">Nom de la personne</a>
+          </h6>
+        <div id="main">
+          <div id="affichageIngrédients">
+            <p id="ensParts"><input type="number" id="nbParts" 
+                min="1" max="100" step="1" value={nbPartsInitiale}/> parts</p>
+            <h4>Ingrédients :</h4>
+            <p id="listeIngredients">
+              <ul>
+                {listIng}
+              </ul>
+            </p>
+          </div>
+          <div id="affichageRecette">
+            <p>Bonjour ! Voici les étapes de la recette !</p>
+          </div>
+        </div>
 
         <style jsx>{`
           #fiche_container {
             margin-top: 10px;
-            border: 1px solid;
-            border-radius: 10px;
             padding: 10px;
-            width: 50%;
+            width: 60%;
           }
+
           h1 {
             text-align : center; 
           }
+
+          h4 {
+            text-decoration: underline;
+            text-align : center;
+          }
+
+          #main {
+            width: 100%;
+          }
+
+          #affichageIngrédients {
+            border: 1px solid;
+            border-radius: 10px;
+            float: left;
+            width: ${pourcentageIngredients}%;
+            margin-right: ${espaceIngredientsRecette-1}px;
+            padding: 10px;
+          }
+
+          #listeIngrédients {
+            text-align : left;
+          }
+
+          #affichageRecette {
+            border: 1px solid;
+            border-radius: 10px;
+            padding: 10px;
+            margin-left: ${pourcentageIngredients+1}%;
+          }
+
+          #nomPersonne {
+            text-align : right;
+            text-decoration : underline;
+          }
+
+          #nbParts {
+            width: 25%;
+          }
+
+          #ensParts {
+            text-align: center;
+          }
+
         `}</style>
       </div>
     )
